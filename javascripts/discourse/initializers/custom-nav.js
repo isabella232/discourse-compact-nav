9aa28c4e89c06c22c77caef96e9f16609f645083
Jordan Vidrine <jordan@jordanvidrine.com>
2020-11-30 14:59:47 -0600

    refactor 2

--- javascripts/discourse/initializers/custom-nav.js
+++ javascripts/discourse/initializers/custom-nav.js
@@ -1,11 +1,12 @@
 import { apiInitializer } from "discourse/lib/api";
+import { alias } from "@ember/object/computed";
 import discourseComputed from "discourse-common/utils/decorators";
 import { inject as service } from "@ember/service";
 
 export default apiInitializer("0.8", api => {
   api.modifyClass("component:navigation-bar", {
-    classNameBindings: ["isCustomNav"],
-    showCustomVersion: "isCustomNav",
+    classNameBindings: ["isCustomNav:custom-version"],
+    showCustomVersion: alias("isCustomNav"),
     router: service("router"),
 
     @discourseComputed("site.isMobileDevice", "router.currentRouteName")
