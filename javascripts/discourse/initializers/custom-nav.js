ace4c02b25c50726b4723a251d757aba10a6c29d
Jordan Vidrine <jordan@jordanvidrine.com>
2020-12-02 12:58:56 -0600

    Refactoring and adding settings

--- javascripts/discourse/initializers/custom-nav.js
+++ javascripts/discourse/initializers/custom-nav.js
@@ -5,15 +5,48 @@ import { inject as service } from "@ember/service";
 
 export default apiInitializer("0.8", api => {
   api.modifyClass("component:navigation-bar", {
-    classNameBindings: ["isCustomNav:custom-version"],
-    showCustomVersion: alias("isCustomNav"),
+    classNameBindings: ["showCompactNav:compact-version"],
+    showCompactVersion: alias("showCompactNav"),
     router: service("router"),
 
     @discourseComputed("site.isMobileDevice", "router.currentRouteName")
-    isCustomNav(isMobile, routeName) {
-      let isTagPage = routeName.split(".")[0] === "tag" || 
+    showCompactNav(isMobile, routeName) {
+
+      let showOnHome = settings.on_home_page;
+      let showOnTag = settings.on_tag_pages;
+      let showOnCategory = settings.on_category_pages;
+
+      let isTagPage = showOnTag === true &&
+        routeName.split(".")[0] === "tag" || 
         routeName.split(".")[0] === "tags";
-      return isMobile || isTagPage;
+
+      let isHomePage = showOnHome === true &&
+        routeName === "discovery.latest" || 
+        routeName === "discovery.top" ||
+        routeName === "discovery.topAll" ||
+        routeName === "discovery.topYearly" ||
+        routeName === "discovery.topQuarterly" ||
+        routeName === "discovery.topMonthly" ||
+        routeName === "discovery.topWeekly" ||
+        routeName === "discovery.topDaily" ||
+        routeName === "discovery.unread" ||
+        routeName === "discovery.new";
+
+      let isCategoryPage = showOnCategory === true &&
+        routeName === "discovery.category" ||
+        routeName === "discovery.latestCategory" ||
+        routeName === "discovery.categories" ||
+        routeName === "discovery.topCategory" ||
+        routeName === "discovery.topAllCategory" ||
+        routeName === "discovery.topYearlyCategory" ||
+        routeName === "discovery.topQuarterlyCategory" ||
+        routeName === "discovery.topMonthlyCategory" ||
+        routeName === "discovery.topWeeklyCategory" ||
+        routeName === "discovery.topDailyCategory" ||
+        routeName === "discovery.categoryAll" ||
+        routeName === "discovery.categoryNone";
+      
+      return isMobile || isTagPage || isHomePage || isCategoryPage;
     }
   });
 });
